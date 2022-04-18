ec0fec6c1fd578be8dea428db82b18feb7942a93
Rachid Agha <rachid845@yahoo.it>
2022-04-18 03:34:16 +0200

    added connection to postgreSQL

---
+++ fastapi/app/main.py
@@ -0,0 +1,60 @@
+
+import time
+from fastapi import FastAPI, Response, status, HTTPException
+from fastapi.params import Body
+from pydantic import BaseModel
+import psycopg
+from psycopg.rows import dict_row
+
+
+app = FastAPI()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+
+
+while True:
+    try:
+        conn = psycopg.connect(
+            host='localhost', dbname='fastapi', user='postgres', password='', row_factory=dict_row, autocommit=True)
+        cursor = conn.cursor()
+        print("Database connection was succesfull")
+        break
+    except Exception as error:
+        print("connecting to database failed")
+        print("Error: ", error)
+        time.sleep(5)
+
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/posts")
+def get_posts():
+    cursor.execute("SELECT * FROM posts")
+    posts = cursor.fetchall()
+    return {"data": posts}
+
+
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
+def create_posts(post: Post):
+    cursor.execute(
+        "INSERT INTO posts (title, content, published) VALUES (%s, %s, %b) RETURNING *",
+        (post.title, post.content, post.published))
+    new_posts = cursor.fetchone()
+    return {"data": new_posts}
+
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    cursor.execute("SELECT * FROM posts WHERE id = %s", (str(id),))
+    post = cursor.fetchone()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id : {id} was not found")
+    return {"data": post}
